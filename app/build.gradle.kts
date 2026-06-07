491d40fef570bf3b359272a6fdeb4b7bbc2ca8f2
Sahil Patil <sahil4683@gmail.com>
2026-06-07 23:41:12 +0530

    skip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
+    }
 }
 
 dependencies {
