70a28793ea4fcc9a8c8978ffa8f97c2ceb43613c
Sahil Patil <sahil4683@gmail.com>
2026-06-07 23:44:51 +0530

    test

--- app/src/test/java/com/videocleaner/data/HashUtilsTest.kt
+++ app/src/test/java/com/videocleaner/data/HashUtilsTest.kt
@@ -31,7 +31,7 @@ class HashUtilsTest {
 
     @Test
     fun `similarity of identical hashes is 100 percent`() {
-        val hash = 0xDEADBEEFCAFEBABEL
+        val hash = 0xDEADBEEFCAFEBABEUL.toLong()
         val similarity = PerceptualHash.similarityPercent(hash, hash)
         assertThat(similarity).isEqualTo(100f)
     }
@@ -56,7 +56,7 @@ class HashUtilsTest {
     @Test
     fun `hamming distance is symmetric`() {
         val hash1 = 0x1234567890ABCDEFL
-        val hash2 = 0xFEDCBA9876543210L
+        val hash2 = 0xFEDCBA9876543210UL.toLong()
         assertThat(PerceptualHash.hammingDistance(hash1, hash2))
             .isEqualTo(PerceptualHash.hammingDistance(hash2, hash1))
     }
@@ -64,7 +64,7 @@ class HashUtilsTest {
     @Test
     fun `similarity within 90 percent threshold for near identical hashes`() {
         // Hashes differing by at most 6 bits should be >= 90% similar
-        val base = 0b1111111111111111111111111111111111111111111111111111111111111111L
+        val base = 0b1111111111111111111111111111111111111111111111111111111111111111UL.toLong()
         val nearIdentical = base xor 0b111111L // 6 bits different
         val similarity = PerceptualHash.similarityPercent(base, nearIdentical)
         assertThat(similarity).isAtLeast(90f)
